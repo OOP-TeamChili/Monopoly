82fc7678fb7b33eec12850366e75e61f9396c0c7
Vasil Sabev <vasil.sabev@gmail.com>
2015-03-24 15:25:49 +0200

    Added classes and enumerations
    Added Class Property, Street, Building; Enum - Color, Building Type

---
+++ Monopoly/Property.cs
@@ -0,0 +1,139 @@
+﻿namespace Monopoly
+{
+    using System;
+
+    public abstract class Property
+    {
+        private const decimal MinPropertyPrice = 1;
+        private const decimal MinMortgageValue = 1;
+        private const decimal MinRentPrice = 1;
+
+        private string name;
+        private decimal buyingPrice;
+        private decimal sellingPrice;
+        private decimal mortgageValue;
+        private decimal rent;
+        private bool mortgaged;
+        private bool owned;
+
+        //the idea is - all prices to be created when the object is created, as everything like price, name and etc is given in the beginning of the game.
+        public Property(string currentName, decimal currentPrice, decimal currentMortgageValue, decimal currentRent)
+        {
+            this.Name = currentName;
+            this.BuyingPrice = currentPrice;
+            this.SellingPrice = this.BuyingPrice / 2;
+            this.MortgageValue = currentMortgageValue;
+            this.Rent = currentRent;
+            this.Mortgaged = false;
+            this.Owned = false;
+        }
+
+        //the set of Name and prices will be private because the values cannot change in the game. 
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                if (String.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Property name cannot be null or empty");
+                }
+
+                this.name = Name;
+            }
+        }
+
+        public decimal BuyingPrice
+        {
+            get
+            {
+                return this.buyingPrice;
+            }
+            private set
+            {
+                if (value < MinPropertyPrice)
+                {
+                    throw new ArgumentOutOfRangeException("The property buying prices cannot be lower than {0}", MinPropertyPrice.ToString());
+                }
+
+                this.buyingPrice = value;
+            }
+        }
+
+        public decimal Rent
+        {
+            get
+            {
+                return this.rent;
+            }
+            private set
+            {
+                if (value < MinRentPrice)
+                {
+                    throw new ArgumentOutOfRangeException("The Rent cannot be lower than {0}", MinRentPrice.ToString());
+                }
+
+                this.rent = value;
+            }
+        }
+
+        public decimal SellingPrice
+        {
+            get
+            {
+                return this.sellingPrice;
+            }
+            private set
+            {
+                //no check here, because the selling prices is calculated, besed on buying price, if buying prices is ok, no need to check here.
+                this.sellingPrice = value;
+            }
+        }
+
+        public decimal MortgageValue
+        {
+            get
+            {
+                return this.mortgageValue;
+            }
+            private set
+            {
+                if (value < MinMortgageValue)
+                {
+                    throw new ArgumentOutOfRangeException("Mortgage value cannot be 0 or negative");
+                }
+
+                this.mortgageValue = value;
+            }
+        }
+        private bool Mortgaged
+        {
+            get
+            {
+                return this.mortgaged;
+            }
+            set
+            {
+                this.mortgaged = value;
+            }
+        }
+
+        private bool Owned
+        {
+            get
+            {
+                return this.owned;
+            }
+            set
+            {
+                this.owned = value;
+            }
+        }
+
+        
+       
+    }
+}
---
+++ Monopoly/Street.cs
@@ -0,0 +1,63 @@
+﻿namespace Monopoly
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Street : Property
+    {
+        private const byte MinBuildings = 0;
+        private const byte MaxBuildings = 4;
+
+        private List<Building> buildings;
+        private Color color;
+
+        public Street(string currentName, Color currentColor, decimal currentPrice, decimal currentMortgageValue, decimal currentRent, decimal currentBuildingBuyPrice, decimal currentBuildingSellPrice)
+            : base(currentName, currentPrice, currentMortgageValue, currentRent)
+        {
+            this.Color = currentColor;
+            this.buildings = new List<Building>();            
+        }
+
+
+        public Color Color
+        {
+            get
+            {
+                return this.color;
+            }
+            private set
+            {
+                //no check - Color is enum
+                this.color = value;
+            }
+        }
+
+        public byte NumberOfBuildings
+        {
+            get
+            {
+                return (byte)buildings.Count;
+            }
+        }
+
+        public List<Building> Buildings
+        {
+            get
+            {
+                return new List<Building>(buildings);
+            }
+        }
+
+        public void Build(Building building)
+        {
+            this.buildings.Add(building);
+        }
+
+        public void Demolish(Building building)
+        {
+            this.buildings.Remove(building);
+        }
+
+        
+    }
+}
